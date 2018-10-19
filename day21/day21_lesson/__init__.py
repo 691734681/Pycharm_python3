652d3271257f95904867e8b7b1ced9ad946de09a
691734681 <691734681@qq.com>
2018-10-19 21:33:32 +0800

    delete all

--- day21/day21_lesson/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/local/python3.5/bin
-# -*-  coding:utf-8 -*-
-
--- day22/pickle&shelve模块.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/python3.5/bin
-# -*-  coding:utf-8 -*-
-
-# pickle模块
-import pickle
-
-# pickle中的方法和使用和json模块一样
-# 区别是json格式是字符串，pickle的格式是字节
-
