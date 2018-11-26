76f4e82c28a84ad6fb3b0c3cded1452a842295e1
691734681 <691734681@qq.com>
2018-11-26 09:45:06 +0800

    test conflict

--- test.py
+++ test.py
@@ -3,8 +3,34 @@
 
 import requests
 import json
+#
+# ip = input("enter ip address:")
+# url = 'http://ip.taobao.com/service/getIpInfo.php?ip='+ip
+# print(url)
+# r = requests.get(url)
+# print(type(r.json()))
+# print(json.dumps(r.json(),ensure_ascii = False,indent = 5))
+# print(r.json()['data']['country'])
 
-r = requests.get('http://ip.taobao.com/service/getIpInfo.php?ip=115.239.211.112')
+# data = dict(name='a_kui',email='a_kui@test.com')
+# res = requests.post("http://httpbin.org/post",data = data)
+# print(res.text)
 
-print(r.text)
-print(json.dumps(r.json(),ensure_ascii = False, indent = 5))
+# r = requests.get('http://httpbin.org/redirect/3')
+# l = r.history
+# print(l)
+# for i in l:
+#     print(i.url[18:])
+# print(r.url[18:])
+
+# print(json.dumps(r.json(),ensure_ascii=False,indent=5))
+
+l = []
+l.append('name1=sb1')
+l.append('name2=sb2')
+print(l)
+s = '&'.join(l)
+url = 'http://httpbin.org/cookies/set?'+s
+print(url)
+r = requests.get(url)
+print(r.json())
