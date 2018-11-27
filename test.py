e46e00fa3bc993af4d0cdec286e467968a0f66a7
691734681 <691734681@qq.com>
2018-11-27 14:51:46 +0800

    Merge branch 'master' of https://github.com/691734681/Pycharm_python3
    # Conflicts:
    #	test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
-
-
-print('test conflict')
-
-print('test conflict')
+principal = 1000
+rate = 0.05
+year = 1
+numberyear = 5
+while year <= numberyear:
+    principal = principal * (1 + rate)
+    print('%3d  %0.2f' % (year,principal))
+    year += 1
 
