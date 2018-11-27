85981aadd91041784b32bfd4d4f41a75ea758fd5
691734681 <691734681@qq.com>
2018-11-27 13:56:37 +0800

    Merge branch 'master' of https://github.com/691734681/Pycharm_python3
    # Conflicts:
    #	test.py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
-from functools import reduce
-l = [1,2,3,4,5]
-print(reduce(lambda x,y:x - y,l))
 
-print(['a' for i in range(10)])
+
+print('test conflict')
 
 print('test conflict')
 
