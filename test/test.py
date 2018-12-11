824d5e320b1e8cc806bfdb446808cca8c7637e65
691734681 <691734681@qq.com>
2018-12-11 10:37:46 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,34 +1,7 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
-import pprint
+n = 100
 
-f = open('test')
-datas = f.readlines()
-print(type(datas))
-pprint.pprint(datas)
-f.close()
-
-print('*' * 60)
-print('*' * 60)
-print('*' * 60)
-print('*' * 60)
-print('*' * 60)
-
-l = []
-
-for data in datas:
-    if data in l:
-        continue
-    else:
-        l.append(data)
-
-pprint.pprint(l)
-
-f = open('test2.txt','w',encoding = 'utf-8')
-for i in l:
-    f.write(i)
-
-
-
-
+def foo():
+    print('it is foo and n is %s' % n)
---
+++ test/test2.py
@@ -0,0 +1,6 @@
+#!/usr/local/python3.5/bin
+# -*-  coding:utf-8 -*-
+
+from test import n
+
+print(n)
