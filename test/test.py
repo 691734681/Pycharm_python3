5f56c486a7ca0c6ffc74bae49a989cf0600f566e
691734681 <691734681@qq.com>
2018-12-14 16:42:44 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,5 +1,15 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
-import pickle
+class TestA:
+    attr = 1
+    def __init__(self):
+        self.attr = 42
+
+obj_a = TestA()
+print(obj_a.attr)
+obj_b = TestA()
+print(obj_b.attr)
+
+
 
