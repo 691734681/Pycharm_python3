3937e85cb266f263c12b8388125581eda363ca2c
691734681 <691734681@qq.com>
2018-12-17 15:55:21 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,15 +1,42 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
-class TestA:
-    attr = 1
-    def __init__(self):
-        self.attr = 42
+import string
+import random
+import time
 
-obj_a = TestA()
-print(obj_a.attr)
-obj_b = TestA()
-print(obj_b.attr)
+# l = []
+# for i in range(97,123):
+#     l.append(chr(i))
+
+l = [chr(i) for i in range(97,123)]
+
+l.append(' ')
+
+print(l)
+
+goal = 'methinks it is a weasel'
+# goal = 'abc'
+
+start_time = time.time()
+
+while True:
+    t_list = []
+    for i in range(len(goal)):
+        t_list.append(random.choice(l))
+    t_string = ''.join(t_list)
+    if t_string == goal:
+        print(t_string)
+        break
+    print(t_string)
+    continue
+
+stop_time = time.time()
+
+t = stop_time - start_time
+print(t)
+
+
 
 
 
