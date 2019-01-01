7cef18a1b656e3ac378aeb0625ecc9e2a61efdda
691734681 <691734681@qq.com>
2019-01-01 19:59:47 +0800

    test.py

--- day22/re模块.py
+++ day22/re模块.py
@@ -1,2 +1,4 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
+
+# 12+(34*6+2-5*(2-1))
--- test/test2.py
+++ test/test2.py
@@ -1,3 +1,7 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
+import re
+
+ret = re.findall('abc{1,4}?','sfojewifoabcccc')
+print(ret)
