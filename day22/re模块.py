08a08c3fd857fefdfdce8e82ae18dfb494526c30
691734681 <691734681@qq.com>
2019-01-02 15:14:18 +0800

    test

--- day22/re模块.py
+++ day22/re模块.py
@@ -2,3 +2,27 @@
 # -*-  coding:utf-8 -*-
 
 # 12+(34*6+2-5*(2-1))
+
+import re
+
+# 匹配（2-1）
+res = re.findall('\([^()]+\)','12+(34*6+2-5*(2-1))')
+print(res)
+
+
+"""
+分组的原则
+1. 如果没有括号则返回匹配到的所有内容
+2. 如果有一个括号则返回匹配到的内容中括号所包含的内容
+3. 有几个括号则返回几个内容
+"""
+s = 'abc def hij klm nop qrs uvw'
+res = re.findall('\w+\s+\w+',s)
+print(res)
+res = re.findall('(\w+)\s+\w+',s)
+print(res)
+res = re.findall('((\w+)\s+\w+)',s)
+print(res)
+
+
+
--- day22/xml模块.py
+++ day22/xml模块.py
@@ -1,6 +1,40 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
+import xml.etree.ElementTree as ET
+
+"""
+# 遍历xml文档
+tree = ET.parse('test.xml')  # 解析xml文档获取树对象
+root = tree.getroot() # 获取根对象
+print(root.tag) # 打印根标签
+
+for i in root:
+    print(i.tag,' * ',i.attrib)
+    for j in i:
+        print('    ',j.tag,' * ',j.attrib,' * ',j.text)
+"""
+
+
+
+
+"""
+# iter()方法返回root下所有节点，可以直接遍历所有节点元素
+
+# # 循环遍历root
+# def view_xml(root):
+#     for i in root.iter():
+#         print(i.tag,' * ',i.attrib,' * ',i.text)
+#
+# view_xml(root)
+"""
+
+
+
+
+
+
+
 
 
 
