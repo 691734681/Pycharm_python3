f8f0dcb4320140fcc74b03458a1f82d52075a65e
691734681 <691734681@qq.com>
2019-01-02 16:58:20 +0800

    test

--- day22/re模块.py
+++ day22/re模块.py
@@ -16,13 +16,18 @@ print(res)
 2. 如果有一个括号则返回匹配到的内容中括号所包含的内容
 3. 有几个括号则返回几个内容
 """
-s = 'abc def hij klm nop qrs uvw'
-res = re.findall('\w+\s+\w+',s)
-print(res)
-res = re.findall('(\w+)\s+\w+',s)
-print(res)
-res = re.findall('((\w+)\s+\w+)',s)
-print(res)
+# s = 'abc def hij klm nop qrs uvw'
+# # res = re.findall('\w+\s+\w+',s)
+# # print(res)
+# # res = re.findall('(\w+)\s+\w+',s)
+# # print(res)
+# # res = re.findall('((\w+)\s+\w+)',s)
+# # print(res)
+
+# 分组的另一种写法
+s = 'alex36wu34lin33yuan35'
+res = re.search('(?P<name>([a-z]+))',s)
+print(res.group('name'))
 
 
 
