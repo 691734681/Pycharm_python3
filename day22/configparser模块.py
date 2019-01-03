f1c1163ca88c7e755dfc1af552fa3c3a362d2103
691734681 <691734681@qq.com>
2019-01-03 14:44:13 +0800

    test

--- day22/configparser模块.py
+++ day22/configparser模块.py
@@ -6,6 +6,35 @@ import configparser
 
 config = configparser.ConfigParser() #创建configParser类对象
 
+# 增
+config.read('dbconf.ini')
+config.add_section('Access')
+config.set('Access','user','admin')
+config.write(open('dbconf.ini','w'))
+
+"""
+# 删
+config.read('dbconf.ini')
+config.remove_section('sqlserver') # 删除指定的名字的第一层所有内容
+config.remove_option('oracle','test') # 删除指定层下的指定键值对
+config.write(open('dbconf.ini','w'))
+"""
+
+"""
+# 改
+config.read('dbconf.ini')
+for i in config:
+    if i == 'DEFAULT':
+        continue
+    for j in config[i]:
+        if j == 'user':
+            continue
+        else:
+            #config[i][j] = 'admin' 
+            config.set(i,j,'admin') # 和上面一句作用相同，使用set方法，第一参数是第一层的key,第二个参数是第二层的key
+
+config.write(open('dbconf.ini','w'))
+"""
 
 """
 # 查
