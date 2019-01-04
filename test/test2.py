5b2e1df72bd2909a51b6741d812061100dab7b83
691734681 <691734681@qq.com>
2019-01-04 16:46:44 +0800

    test

--- test/test2.py
+++ test/test2.py
@@ -3,5 +3,27 @@
 
 import re
 
-ret = re.findall('abc{1,4}?','sfojewifoabcccc')
-print(ret)
+s = '1 + 2 * ( 4 + ( 2 + 1 + 3 + 2 ) )'
+s = s.replace(' ','')
+print(s)
+
+reg = r'\([^()]+\)'
+
+res = re.search(reg,s)
+temp = res.group()
+temp = temp[1:-1]
+print(temp)
+while len(temp) >= 3:
+    sum = 0
+    if temp[1] == '+':
+        sum = int(temp[0]) + int(temp[2])
+    temp = str(sum) + temp[3:]
+s = re.sub(reg,temp,s)
+
+
+
+
+
+
+
+
