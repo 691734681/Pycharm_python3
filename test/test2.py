bfbf5a90197dc7278c388d2c39ecdc0e62895887
691734681 <691734681@qq.com>
2019-01-08 16:23:00 +0800

    test

--- test/test2.py
+++ test/test2.py
@@ -3,8 +3,25 @@
 
 import re
 
-s = '1 + 2 * ( 4 + ( 2 + 2 ) + ( 1 + 2 ))'
-s2 = re.sub('\s+','',s)
+s1 = '1 + 2 * ( 4 + ( 2 + 2 ) + ( 1 + 2 ))'
+s2 = re.sub('\s+','',s1)
+print(s2)
+# s3 = re.search('\([^()]+\)',s2)
+# print(s3)
+
+def qu_kuo_hao(s):
+    """qu_kuo_hao"""
+    while True:
+        res = re.search('\([^()]+\)',s)
+        if not res:
+            break
+        print(res)
+
+    return res
+
+qu_kuo_hao(s2)
+
+
 
 
 
