754a0677e8880660458c3075583e71dd2893d30e
691734681 <691734681@qq.com>
2019-01-11 16:20:30 +0800

    test

--- test/test3.py
+++ test/test3.py
@@ -1,6 +1,27 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
-import os
-import pprint
+class Test:
+    """Test"""
+    def __init__(self,value):
+        self.value = value
+
+    def __add__(self, other):
+        # self.value = self.value + other
+        return Test(self.value + other)
+
+    def __mul__(self, other):
+        # self.value = self.value * other
+        return Test(self.value * other)
+
+t = Test('a')
+t += 'b'
+print(t.value)
+t2 = Test('c')
+t2 *= 10
+print(t2.value)
+
+
+
+
 
