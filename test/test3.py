52d14f169df367931d4c57e5257501e2a210a62a
691734681 <691734681@qq.com>
2019-01-11 16:49:39 +0800

    test

--- test/test3.py
+++ test/test3.py
@@ -2,26 +2,36 @@
 # -*-  coding:utf-8 -*-
 
 class Super:
+    """super"""
     def method(self):
+        """method"""
         print('in super.method')
     def delegate(self):
+        """delegate"""
         self.action()
 
 class Inheritor(Super):
+    """inheritor"""
     pass
 
 class Replacer(Super):
+    """replacer"""
     def method(self):
+        """method"""
         print('in replace.method')
 
 class Extender(Super):
+    """extender"""
     def method(self):
+        """method"""
         print('starting extender.method')
         super.method()
         print('ending extender.method')
 
 class Provider(Super):
+    """provider"""
     def action(self):
+        """action"""
         print('in provider.action')
 
 if __name__ == __main__:
