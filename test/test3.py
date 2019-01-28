ea7af270018c038cfa141ca6663ccaa8e06c7e16
691734681 <691734681@qq.com>
2019-01-28 16:34:14 +0800

    test

--- test/test3.py
+++ test/test3.py
@@ -1,6 +1,21 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
+class FirstClass:
+    def __init__(self, value):
+        self.data = value
+    def display(self):
+        print(self.data)
+    def __add__(self,other):
+        return FirstClass(self.data + other)
+    def __mul__(self, other):
+        self.data = self.data * other
 
+f1 = FirstClass('abc')
+f2 = f1 + 'xyz'
+f3 = f1 * 3
 
-
+# print(type(f1),'*****',f1.display())
+# print(type(f2),'*****',f2.display())
+# print(type(f3),'*****',f3.display())
+print(f1)
