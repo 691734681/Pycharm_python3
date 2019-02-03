77a396a77cacd2776bd3992ca4dd5a23f139b400
691734681 <691734681@qq.com>
2019-02-03 13:09:15 +0800

    test

--- test/test3.py
+++ test/test3.py
@@ -1,4 +1,18 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
-int
+import pprint
+import tokenize
+
+reader = open('bonus.py').__next__
+print(type(reader),' * ',reader)
+tokens = tokenize.generate_tokens(reader)
+print(type(tokens),' * ',tokens)
+print('*****************')
+l = [tokens.__next__() for i in range(10)]
+pprint.pprint(l)
+
+
+
+
+
