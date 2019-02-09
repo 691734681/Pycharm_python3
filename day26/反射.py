a11c4516a842db9ec46830edfdf3e7715518cfe5
691734681 <691734681@qq.com>
2019-02-09 23:22:34 +0800

    test.py

--- day26/反射.py
+++ day26/反射.py
@@ -1,2 +1,4 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
+
+
--- test/bonus.py
+++ test/bonus.py
@@ -1,17 +1,40 @@
 #!/usr/local/python3.5/bin
 # -*-  coding:utf-8 -*-
 
+# 2,5,6,8,10,12,22,23  ###  7,8,9,10,11,19,22,25   ###   2,5,6,7,8,9,10,11,12,19,22,23,25
+
 import random
-l = []
-while True:
-    if len(l) == 6:
-        break
-    t = random.randint(1,34)
-    if t in l:
-        continue
-    else:
-        l.append(t)
-l.sort()
-l.append(random.randint(1,17))
-print(l)
+l1 = [2,5,6,8,10,12,22,23]
+l2 = [7,8,9,10,11,19,22,25]
+l3 = [2,5,6,7,8,9,10,11,12,19,22,23,25]
+
+def getBonus(l):
+    """bonus"""
+    red = random.sample(l,6)
+    red.sort()
+    blue = random.sample(l[0:-3],2)
+    blue.sort()
+    return red,blue
+
+print(getBonus(l1))
+print(getBonus(l2))
+
+def getBonus2(l,i,j):
+    """bonus"""
+    red = random.sample(l,i)
+    red.sort()
+    blue = random.sample(l[0:9],j)
+    blue.sort()
+    return red,blue
+
+print(getBonus2(l3,7,2))
+print(getBonus2(l3,7,3))
+
+
+
+
+
+
+
+
 
